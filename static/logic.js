fc0fb5b8ad79b90412dfb1ffeefe215d05556286
cnlamanna <cnlamanna@gmail.com>
2023-08-24 18:45:16 -0500

    push

--- static/logic.js
+++ static/logic.js
@@ -2,13 +2,13 @@
 var map = new ol.Map({
   target: 'map',
   layers: [
-      new ol.layer.Tile({
-          source: new ol.source.OSM() // OpenStreetMap as the base layer
-      })
+    new ol.layer.Tile({
+      source: new ol.source.OSM() // OpenStreetMap as the base layer
+    })
   ],
   view: new ol.View({
-      center: ol.proj.fromLonLat([-77.25183283, 39.15403667]), // Coordinates (longitude, latitude)
-      zoom: 15 // Zoom level
+    center: ol.proj.fromLonLat([-77.25183283, 39.15403667]), // Coordinates (longitude, latitude)
+    zoom: 15 // Zoom level
   })
 });
 
@@ -35,13 +35,29 @@ d3.json('/geomap').then(data => {
 
       // Add click event listener to each marker
       marker.on('click', function () {
+          console.log('Marker clicked!', crash);
           // Update the markerInfo element with the crash data
           var markerInfo = document.getElementById('markerInfo');
           markerInfo.innerHTML = `
-              <h2>Marker Information</h2>
-              <p>Report Number: ${crash["Report Number"]}</p>
-              <p>Report Type: ${crash["Report Type"]}</p>
-              <!-- Include other relevant data here -->
+          <h2>Marker Information</h2>
+          <p>Report Number: ${crash["Report Number"]}</p>
+          <p>Report Type: ${crash["Report Type"]}</p>
+          <p>Date & Time: ${crash["Date & Time"]}</p>
+          <p>Weather: ${crash["Weather"]}</p>
+          <p>Light: ${crash["Light"]}</p>
+          <p>Latitude: ${crash["Latitude"]}</p>
+          <p>Longitude: ${crash["Longitude"]}</p>
+          <p>Vehicle Report Number: ${crash["Vehicle Report Number"]}</p>
+          <p>Vehicle ID: ${crash["Vehicle_ID"]}</p>
+          <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
+          <p>Body Type: ${crash["Body Type"]}</p>
+          <p>Year: ${crash["Year"]}</p>
+          <p>Make: ${crash["Make"]}</p>
+          <p>Model: ${crash["Model"]}</p>
+          <p>Driver Report Number: ${crash["Driver Report Number"]}</p>
+          <p>Substance Abuse: ${crash["SubstanceAbuse"]}</p>
+          <p>Person ID: ${crash["Person_ID"]}</p>
+          <p>Injury Severity: ${crash["Injury Severity"]}</p>
           `;
       });
   });
