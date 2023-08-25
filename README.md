2da6e5629da4dfdc67511f5b04eeeefbe00e3d95
cnlamanna <cnlamanna@gmail.com>
2023-08-24 20:39:46 -0500

    added dropdown

--- README.md
+++ README.md
@@ -39,3 +39,112 @@
 ### Conclusion:
 #### The proposed interactive car crash map will provide a user-friendly platform for visualizing and understanding car crash data in Montgomery County. By leveraging real-time data and advanced visualization techniques, this map aims to raise awareness, enhance road safety, and contribute to informed decision-making in the community.
 
+
+leaflet works but slow
+// Create a map
+var map = L.map('map').setView([39.15403667, -77.25183283], 15);
+
+// Add OpenStreetMap as the base layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Fetch data for geomap graph
+d3.json('/geomap').then(data => {
+  data.forEach(crash => {
+    var marker = L.marker([crash.Latitude, crash.Longitude]).addTo(map);
+
+    marker.bindPopup(`
+      <h2>Marker Information</h2>
+      <p>Report Type: ${crash["Report Type"]}</p>
+      <p>Date & Time: ${crash["Date & Time"]}</p>
+      <p>Weather: ${crash["Weather"]}</p>
+      <p>Light: ${crash["Light"]}</p>
+      <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
+      <p>Year: ${crash["Year"]}</p>
+      <p>Make: ${crash["Make"]}</p>
+      <p>Model: ${crash["Model"]}</p>
+      <p>Substance Abuse: ${crash["SubstanceAbuse"]}</p>
+      <p>Injury Severity: ${crash["Injury Severity"]}</p>
+    `);
+  });
+});
+
+
+this is the vector one
+// Create a map
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM() // OpenStreetMap as the base layer
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-77.25183283, 39.15403667]), // Coordinates (longitude, latitude)
+    zoom: 15 // Zoom level
+  })
+});
+
+// fetch data for geomap graph
+d3.json('/geomap').then(data => {
+  console.log(data);
+  var vectorSource = new ol.source.Vector(); // Create the vector source outside the loop
+
+  data.forEach(crash => {
+      var marker = new ol.Feature({
+          geometry: new ol.geom.Point(
+              ol.proj.fromLonLat([crash.Longitude, crash.Latitude])
+          )
+      });
+
+      var iconStyle = new ol.style.Style({
+          image: new ol.style.Icon({
+              src: 'https://openlayers.org/en/latest/examples/data/icon.png' // URL to the marker icon
+          })
+      });
+
+      marker.setStyle(iconStyle);
+      vectorSource.addFeature(marker); // Add the marker to the vector source
+
+      // Add click event listener to each marker
+      marker.on('click', function () {
+          console.log('Marker clicked!', crash);
+          // Update the markerInfo element with the crash data
+          var markerInfo = document.getElementById('markerInfo');
+          markerInfo.innerHTML = `
+          <h2>Marker Information</h2>
+          <p>Report Number: ${crash["Report Number"]}</p>
+          <p>Report Type: ${crash["Report Type"]}</p>
+          <p>Date & Time: ${crash["Date & Time"]}</p>
+          <p>Weather: ${crash["Weather"]}</p>
+          <p>Light: ${crash["Light"]}</p>
+          <p>Latitude: ${crash["Latitude"]}</p>
+          <p>Longitude: ${crash["Longitude"]}</p>
+          <p>Vehicle Report Number: ${crash["Vehicle Report Number"]}</p>
+          <p>Vehicle ID: ${crash["Vehicle_ID"]}</p>
+          <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
+          <p>Body Type: ${crash["Body Type"]}</p>
+          <p>Year: ${crash["Year"]}</p>
+          <p>Make: ${crash["Make"]}</p>
+          <p>Model: ${crash["Model"]}</p>
+          <p>Driver Report Number: ${crash["Driver Report Number"]}</p>
+          <p>Substance Abuse: ${crash["SubstanceAbuse"]}</p>
+          <p>Person ID: ${crash["Person_ID"]}</p>
+          <p>Injury Severity: ${crash["Injury Severity"]}</p>
+          `;
+      });
+  });
+
+  var vectorLayer = new ol.layer.Vector({
+      source: vectorSource
+  });
+
+  // Add click event listener to the map
+  map.on('click', function () {
+      var markerInfo = document.getElementById('markerInfo');
+      markerInfo.innerHTML = ''; // Clear the content
+  });
+
+  // Add the vector layer to the map
+  map.addLayer(vectorLayer);
+});
+
--- static/logic.js
+++ static/logic.js
@@ -7,69 +7,96 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([-77.25183283, 39.15403667]), // Coordinates (longitude, latitude)
-    zoom: 15 // Zoom level
+    center: ol.proj.fromLonLat([-77.25183283, 39.15403667]),
+    zoom: 15
   })
 });
 
-// fetch data for geomap graph
+// Fetch data for geomap graph
 d3.json('/geomap').then(data => {
   console.log(data);
-  var vectorSource = new ol.source.Vector(); // Create the vector source outside the loop
+  var vectorSource = new ol.source.Vector();
 
-  data.forEach(crash => {
-      var marker = new ol.Feature({
-          geometry: new ol.geom.Point(
-              ol.proj.fromLonLat([crash.Longitude, crash.Latitude])
-          )
-      });
+  // Create a marker using the createMarker function
+  function createMarker(crash) {
+    var marker = new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat([crash.Longitude, crash.Latitude])
+      )
+    });
 
-      var iconStyle = new ol.style.Style({
-          image: new ol.style.Icon({
-              src: 'https://openlayers.org/en/latest/examples/data/icon.png' // URL to the marker icon
-          })
-      });
+    var iconStyle = new ol.style.Style({
+      image: new ol.style.Icon({
+        src: 'https://openlayers.org/en/latest/examples/data/icon.png'
+      })
+    });
 
-      marker.setStyle(iconStyle);
-      vectorSource.addFeature(marker); // Add the marker to the vector source
+    marker.setStyle(iconStyle);
 
-      // Add click event listener to each marker
-      marker.on('click', function () {
-          console.log('Marker clicked!', crash);
-          // Update the markerInfo element with the crash data
-          var markerInfo = document.getElementById('markerInfo');
-          markerInfo.innerHTML = `
-          <h2>Marker Information</h2>
-          <p>Report Number: ${crash["Report Number"]}</p>
-          <p>Report Type: ${crash["Report Type"]}</p>
-          <p>Date & Time: ${crash["Date & Time"]}</p>
-          <p>Weather: ${crash["Weather"]}</p>
-          <p>Light: ${crash["Light"]}</p>
-          <p>Latitude: ${crash["Latitude"]}</p>
-          <p>Longitude: ${crash["Longitude"]}</p>
-          <p>Vehicle Report Number: ${crash["Vehicle Report Number"]}</p>
-          <p>Vehicle ID: ${crash["Vehicle_ID"]}</p>
-          <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
-          <p>Body Type: ${crash["Body Type"]}</p>
-          <p>Year: ${crash["Year"]}</p>
-          <p>Make: ${crash["Make"]}</p>
-          <p>Model: ${crash["Model"]}</p>
-          <p>Driver Report Number: ${crash["Driver Report Number"]}</p>
-          <p>Substance Abuse: ${crash["SubstanceAbuse"]}</p>
-          <p>Person ID: ${crash["Person_ID"]}</p>
-          <p>Injury Severity: ${crash["Injury Severity"]}</p>
-          `;
-      });
-  });
+    // Add click event listener to each marker
+    marker.on('click', function () {
+      console.log('Marker clicked!', crash);
+      // Update the markerInfo element with the crash data
+      var markerInfo = document.getElementById('markerInfo');
+      markerInfo.innerHTML = `
+        <h2>Marker Information</h2>
+        <p>Report Number: ${crash["Report Number"]}</p>
+        <p>Report Type: ${crash["Report Type"]}</p>
+        <p>Date & Time: ${crash["Date & Time"]}</p>
+        <p>Weather: ${crash["Weather"]}</p>
+        <p>Light: ${crash["Light"]}</p>
+        <p>Latitude: ${crash["Latitude"]}</p>
+        <p>Longitude: ${crash["Longitude"]}</p>
+        <p>Vehicle Report Number: ${crash["Vehicle Report Number"]}</p>
+        <p>Vehicle ID: ${crash["Vehicle_ID"]}</p>
+        <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
+        <p>Body Type: ${crash["Body Type"]}</p>
+        <p>Year: ${crash["Year"]}</p>
+        <p>Make: ${crash["Make"]}</p>
+        <p>Model: ${crash["Model"]}</p>
+        <p>Driver Report Number: ${crash["Driver Report Number"]}</p>
+        <p>Substance Abuse: ${crash["SubstanceAbuse"]}</p>
+        <p>Person ID: ${crash["Person_ID"]}</p>
+        <p>Injury Severity: ${crash["Injury Severity"]}</p>
+      `;
+    });
+
+    return marker;
+  }
+
+  // Function to update map markers based on selected week
+  function updateMapMarkers() {
+    var dropdown = document.getElementById('weekDropdown');
+    var selectedWeek = dropdown.value;
+
+    // Clear existing markers
+    vectorSource.clear();
+
+    // Filter data for the selected week
+    var selectedWeekData = data.filter(crash => {
+      var crashDate = new Date(crash["Date & Time"]);
+      var weekNumber = Math.ceil(crashDate.getDate() / 7);
+      return selectedWeek === 'week' + weekNumber;
+    });
+
+    // Add markers for the selected week
+    selectedWeekData.forEach(crash => {
+      var marker = createMarker(crash);
+      vectorSource.addFeature(marker);
+    });
+  }
+
+  // Call the function initially to load markers for the first week
+  updateMapMarkers();
 
   var vectorLayer = new ol.layer.Vector({
-      source: vectorSource
+    source: vectorSource
   });
 
   // Add click event listener to the map
   map.on('click', function () {
-      var markerInfo = document.getElementById('markerInfo');
-      markerInfo.innerHTML = ''; // Clear the content
+    var markerInfo = document.getElementById('markerInfo');
+    markerInfo.innerHTML = '';
   });
 
   // Add the vector layer to the map
