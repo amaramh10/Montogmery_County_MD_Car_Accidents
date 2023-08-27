16fd57129b8b1e2a74065058365903041f5eca6c
cnlamanna <cnlamanna@gmail.com>
2023-08-26 23:10:39 -0500

    optimized leaflet map in app and logic, map is faster and popups work. just finalizing dropdown

--- app.py
+++ app.py
@@ -12,44 +12,70 @@ def index():
    return render_template("index.html")
 # ^DON'T TOUCH THIS ONE^
 
-
-# ROUTE FOR GEO MAP
-@app.route("/geomap")
-def geomap():
+# updated/refined geomap
+@app.route("/optimized_geomap")
+def optimized_geomap():
     conn = psycopg2.connect(database="car_crash", user="postgres",
                         password="postgres", host="localhost", port="5432")
-# create a cursor
     cur = conn.cursor()
-# connect table.
-    cur.execute('''SELECT * FROM "Crash_Data" LEFT JOIN "Vehicle_Information" ON "Crash_Data".report_num = "Vehicle_Information".report_num LEFT JOIN "Driver" ON "Crash_Data".report_num = "Driver".report_num;''')
-    map_data = cur.fetchall()
+    cur.execute('''SELECT * FROM "Optimized_Data";''')
+    refined_data = cur.fetchall()
     cur.close()
     conn.close()
 
-    map_list = []
-    for item in map_data:
-        map_list.append({
-            "Report Number": item[0],
-            "Report Type": item[1],
-            "Date & Time": item[2],
-            "Weather": item[3],
-            "Light": item[4],
-            "Latitude": item[5],
-            "Longitude": item[6],
-            "Report Number": item[7],
-            "Vehicle_ID": item[8],
-            "Vehicle Damage": item[9],
-            "Body Type": item[10],
-            "Year": item[11],
-            "Make": item[12],
-            "Model": item[13],
-            "Report Number": item[14],
-            "SubstanceAbuse": item[15],
-            "Person_ID": item[16],
-            "Injury Severity": item[17]
+    refined_list = []
+    for item in refined_data:
+        refined_list.append({
+            "Date & Time": item[0],
+            "Weather": item[1],
+            "Latitude": item[2],
+            "Longitude": item[3],
+            "Vehicle Damage": item[4],
+            "Year": item[5],
+            "Make": item[6],
+            "Model": item[7],
+            "Substance Abuse": item[8],
+            "Injury Severity": item[9]
         })
-    # return render_template('index.html', data=data)
-    return jsonify(map_list)
+    return jsonify(refined_list)
+
+# # ROUTE FOR GEO MAP
+# @app.route("/geomap")
+# def geomap():
+#     conn = psycopg2.connect(database="car_crash", user="postgres",
+#                         password="postgres", host="localhost", port="5432")
+# # create a cursor
+#     cur = conn.cursor()
+# # connect table.
+#     cur.execute('''SELECT * FROM "Crash_Data" LEFT JOIN "Vehicle_Information" ON "Crash_Data".report_num = "Vehicle_Information".report_num LEFT JOIN "Driver" ON "Crash_Data".report_num = "Driver".report_num;''')
+#     map_data = cur.fetchall()
+#     cur.close()
+#     conn.close()
+
+#     map_list = []
+#     for item in map_data:
+#         map_list.append({
+#             "Report Number": item[0],
+#             "Report Type": item[1],
+#             "Date & Time": item[2],
+#             "Weather": item[3],
+#             "Light": item[4],
+#             "Latitude": item[5],
+#             "Longitude": item[6],
+#             "Report Number": item[7],
+#             "Vehicle_ID": item[8],
+#             "Vehicle Damage": item[9],
+#             "Body Type": item[10],
+#             "Year": item[11],
+#             "Make": item[12],
+#             "Model": item[13],
+#             "Report Number": item[14],
+#             "SubstanceAbuse": item[15],
+#             "Person_ID": item[16],
+#             "Injury Severity": item[17]
+#         })
+#     # return render_template('index.html', data=data)
+#     return jsonify(map_list)
 
 
 
--- static/logic.js
+++ static/logic.js
@@ -1,58 +1,64 @@
-// Create a map
-var map = L.map('map').setView([39.15403667, -77.25183283], 15);
+// Define a function to initialize the map and markers
+function initializeMap() {
+  // Create a map
+  var map = L.map('map').setView([39.15403667, -77.25183283], 15);
 
-// Add OpenStreetMap as the base layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+  // Add OpenStreetMap as the base layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Fetch data for geomap graph
-d3.json('/geomap').then(data => {
-  var markers = []; // Array to hold all markers
-
-  data.forEach(crash => {
-    var marker = L.marker([crash.Latitude, crash.Longitude]);
-    
-    marker.bindPopup(`
-      <h2>Marker Information</h2>
-      <p>Report Type: ${crash["Report Type"]}</p>
-      <p>Date & Time: ${crash["Date & Time"]}</p>
-      <p>Weather: ${crash["Weather"]}</p>
-      <p>Light: ${crash["Light"]}</p>
-      <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
-      <p>Year: ${crash["Year"]}</p>
-      <p>Make: ${crash["Make"]}</p>
-      <p>Model: ${crash["Model"]}</p>
-      <p>Substance Abuse: ${crash["SubstanceAbuse"]}</p>
-      <p>Injury Severity: ${crash["Injury Severity"]}</p>
-    `);
-
-    markers.push(marker); // Add marker to the array
-  });
-
-  // Create a layer group for all markers
-  var markerLayer = L.layerGroup(markers).addTo(map);
-
-  // Dropdown event listener
-  var weekDropdown = document.getElementById('weekDropdown');
-  weekDropdown.addEventListener('change', function () {
-    var selectedWeek = parseInt(weekDropdown.value);
-    updateMapMarkers(selectedWeek);
-  });
-
-  // Function to update markers based on selected week
-  function updateMapMarkers(selectedWeek) {
-    markerLayer.clearLayers(); // Clear existing markers
+  // Fetch data for geomap graph
+  d3.json('/optimized_geomap').then(data => {
+    var markers = []; // Array to hold all markers
 
     data.forEach(crash => {
-      var week = getWeekNumber(new Date(crash["Date & Time"]));
-      if (week === selectedWeek) {
-        markerLayer.addLayer(markers[crash.index]);
-      }
-    });
-  }
+      var marker = L.marker([crash.Latitude, crash.Longitude]);
 
-  // Function to get week number
-  function getWeekNumber(date) {
-    var oneJan = new Date(date.getFullYear(), 0, 1);
-    return Math.ceil(((date - oneJan) / 86400000 + oneJan.getDay() + 1) / 7);
-  }
-});
+      marker.bindPopup(`
+        <h2>Marker Information</h2>
+        <p>Date & Time: ${crash["Date & Time"]}</p>
+        <p>Weather: ${crash["Weather"]}</p>
+        <p>Vehicle Damage: ${crash["Vehicle Damage"]}</p>
+        <p>Year: ${crash["Year"]}</p>
+        <p>Make: ${crash["Make"]}</p>
+        <p>Model: ${crash["Model"]}</p>
+        <p>Substance Abuse: ${crash["Substance Abuse"]}</p>
+        <p>Injury Severity: ${crash["Injury Severity"]}</p>
+      `);
+
+      markers.push({
+        marker: marker, // Store the marker object
+        week: getWeekNumber(new Date(crash["Date & Time"]))
+      });
+    });
+
+    // Create a layer group for all markers
+    var markerLayer = L.layerGroup(markers.map(crash => crash.marker)).addTo(map);
+
+    // Dropdown event listener
+    var weekDropdown = document.getElementById('weekDropdown');
+    weekDropdown.addEventListener('change', function () {
+      var selectedWeek = parseInt(weekDropdown.value);
+      updateMapMarkers(selectedWeek, markers, markerLayer);
+    });
+  });
+}
+
+// Call the initializeMap function when the page loads
+document.addEventListener('DOMContentLoaded', initializeMap);
+
+// Function to update markers based on selected week
+function updateMapMarkers(selectedWeek, markers, markerLayer) {
+  markerLayer.clearLayers(); // Clear existing markers
+
+  markers.forEach(crash => {
+    if (crash.week === selectedWeek) {
+      markerLayer.addLayer(crash.marker); // Use the marker object directly
+    }
+  });
+}
+
+// Function to get week number
+function getWeekNumber(date) {
+  var oneJan = new Date(date.getFullYear(), 0, 1);
+  return Math.ceil(((date - oneJan) / 86400000 + oneJan.getDay() + 1) / 7);
+}
