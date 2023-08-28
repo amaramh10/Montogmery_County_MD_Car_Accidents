a34f607402fcf84724c735a40a7ccbc53f38eee7
cnlamanna <cnlamanna@gmail.com>
2023-08-28 17:04:51 -0500

    showing lines for chart, just going to update visual

--- static/logic.js
+++ static/logic.js
@@ -148,79 +148,82 @@ d3.json('/bubble').then(scatterData => {
 
 
 // line chart 
-d3.json('/line').then(item => {
-  console.log(item);
-  var dom = document.getElementById('linechart-container');
-  var myChart = echarts.init(dom, null, {
-    renderer: 'canvas',
-    useDirtyRect: false
+// Line chart
+d3.json('/week_one').then(weekOneData => {
+  d3.json('/week_two').then(weekTwoData => {
+    d3.json('/week_three').then(weekThreeData => {
+      d3.json('/week_four').then(weekFourData => {
+        console.log(weekOneData);
+        console.log(weekTwoData);
+        console.log(weekThreeData);
+        console.log(weekFourData);
+
+        var dom = document.getElementById('linechart-container');
+        var myChart = echarts.init(dom, null, {
+          renderer: 'canvas',
+          useDirtyRect: false
+        });
+        var app = {};
+        var option;
+
+        option = {
+          title: {
+            text: 'Accidents Per Day'
+          },
+          tooltip: {
+            trigger: 'axis'
+          },
+          legend: {
+            data: ['Week 1', 'Week 2', 'Week 3', 'Week 4']
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: weekOneData.map(item => item.date_only) // Assuming date_only field contains date strings
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [
+            {
+              name: 'Week 1',
+              type: 'line',
+              data: weekOneData.map(item => item.accident_count)
+            },
+            {
+              name: 'Week 2',
+              type: 'line',
+              data: weekTwoData.map(item => item.accident_count)
+            },
+            {
+              name: 'Week 3',
+              type: 'line',
+              data: weekThreeData.map(item => item.accident_count)
+            },
+            {
+              name: 'Week 4',
+              type: 'line',
+              data: weekFourData.map(item => item.accident_count)
+            }
+          ]
+        };
+        if (option && typeof option === 'object') {
+          myChart.setOption(option);
+        }
+        window.addEventListener('resize', myChart.resize);
+      });
+    });
   });
-  var app = {};
-  var option;
-  option = {
-    title: {
-      text: 'Accidents Per Day'
-    },
-    tooltip: {
-      trigger: 'axis'
-    },
-    legend: {
-      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        name: 'Email',
-        type: 'line',
-        stack: 'Total',
-        data: [120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-        name: 'Union Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [220, 182, 191, 234, 290, 330, 310]
-      },
-      {
-        name: 'Video Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [150, 232, 201, 154, 190, 330, 410]
-      },
-      {
-        name: 'Direct',
-        type: 'line',
-        stack: 'Total',
-        data: [320, 332, 301, 334, 390, 330, 320]
-      },
-      {
-        name: 'Search Engine',
-        type: 'line',
-        stack: 'Total',
-        data: [820, 932, 901, 934, 1290, 1330, 1320]
-      }
-    ]
-  };
-  if (option && typeof option === 'object') {
-    myChart.setOption(option);
-  }
-  window.addEventListener('resize', myChart.resize);
 });
+
