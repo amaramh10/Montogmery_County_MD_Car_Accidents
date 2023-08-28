2a5783c8e96760d6d33dac68ac10e1cdd20b1557
kadyepley <131722669+kadyepley@users.noreply.github.com>
2023-08-28 13:47:22 -0400

    Add files via upload

---
+++ accident_data.sql
@@ -0,0 +1,100 @@
+---------------------------------------------------
+-- Crash Data Table
+CREATE TABLE "Crash_Data" (
+    "report_num" VARCHAR   NOT NULL,
+    "report_type" VARCHAR   NOT NULL,
+    "date_time" TIMESTAMP   NOT NULL,
+    "weather" VARCHAR   NOT NULL,
+    "light" VARCHAR   NOT NULL,
+    "latitude" NUMERIC   NOT NULL,
+    "longitude" NUMERIC   NOT NULL
+);
+SELECT * from "Crash_Data"
+---------------------------------------------------
+-- Vehicle Data Table
+CREATE TABLE "Vehicle_Information" (
+    "report_num" VARCHAR NOT NULL,
+    "vehicle_id" VARCHAR   NOT NULL,
+    "vehicle_damage" VARCHAR   NOT NULL,
+    "body_type" VARCHAR,
+    "year" INTEGER   NOT NULL,
+    "make" VARCHAR   NOT NULL,
+    "model" VARCHAR   NOT NULL
+);
+select * FROM "Vehicle_Information"
+---------------------------------------------------
+-- Driver Data Table
+CREATE TABLE "Driver" (
+    "report_num" VARCHAR NOT NULL,
+    "person_id" VARCHAR   NOT NULL,
+    "substance_abuse" VARCHAR   NOT NULL,
+    "injury_severity" VARCHAR   NOT NULL
+select * FROM "Driver"
+);
+---------------------------------------------------
+-- Bubble Chart Data
+CREATE TABLE bubble_data AS
+SELECT
+    EXTRACT(DOW FROM date_time) AS day_numeric,
+    EXTRACT(HOUR FROM date_time) AS hour,
+    CASE
+        WHEN EXTRACT(DOW FROM date_time) IS NOT NULL THEN 1
+        ELSE 0
+    END AS new_column
+FROM
+    "Crash_Data";
+ALTER TABLE bubble_data
+    RENAME COLUMN new_column TO accident_count;
+SELECT * FROM "bubble_data";
+---------------------------------------------------
+-- Week One Total Accidents by Day Table
+CREATE TABLE week_one_totals AS
+SELECT
+  DATE(date_time) AS date_only,
+  SUM(CASE WHEN EXTRACT(DAY FROM date_time) IS NOT NULL THEN 1 ELSE 0 END) AS new_column
+FROM "Crash_Data"
+WHERE date_time BETWEEN '2023-07-02 00:00:00' AND '2023-07-08 23:59:59'
+GROUP BY date_only
+ORDER BY date_only ASC;
+ALTER TABLE week_one_totals
+    RENAME COLUMN new_column TO accident_count;
+SELECT * FROM "week_one_totals";
+---------------------------------------------------
+-- Week Two Total Accidents by Day Table
+CREATE TABLE week_two_totals AS
+SELECT
+  DATE(date_time) AS date_only,
+  SUM(CASE WHEN EXTRACT(DAY FROM date_time) IS NOT NULL THEN 1 ELSE 0 END) AS new_column
+FROM "Crash_Data"
+WHERE date_time BETWEEN '2023-07-09 00:00:00' AND '2023-07-15 23:59:59'
+GROUP BY date_only
+ORDER BY date_only ASC;
+ALTER TABLE week_two_totals
+    RENAME COLUMN new_column TO accident_count;
+SELECT * FROM "week_two_totals";
+---------------------------------------------------
+-- Week Three Total Accidents by Day Table
+CREATE TABLE week_three_totals AS
+SELECT
+  DATE(date_time) AS date_only,
+  SUM(CASE WHEN EXTRACT(DAY FROM date_time) IS NOT NULL THEN 1 ELSE 0 END) AS new_column
+FROM "Crash_Data"
+WHERE date_time BETWEEN '2023-07-16 00:00:00' AND '2023-07-22 23:59:59'
+GROUP BY date_only
+ORDER BY date_only ASC;
+ALTER TABLE week_three_totals
+    RENAME COLUMN new_column TO accident_count;
+SELECT * FROM "week_three_totals";
+---------------------------------------------------
+-- Week Four Total Accidents by Day Table
+CREATE TABLE week_four_totals AS
+SELECT
+  DATE(date_time) AS date_only,
+  SUM(CASE WHEN EXTRACT(DAY FROM date_time) IS NOT NULL THEN 1 ELSE 0 END) AS new_column
+FROM "Crash_Data"
+WHERE date_time BETWEEN '2023-07-23 00:00:00' AND '2023-07-29 23:59:59'
+GROUP BY date_only
+ORDER BY date_only ASC;
+ALTER TABLE week_four_totals
+    RENAME COLUMN new_column TO accident_count;
+SELECT * FROM "week_four_totals";
---
+++ app.py
@@ -0,0 +1,166 @@
+from flask import Flask, render_template, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+#Route to produce your main home page. 
+#Feel free to add additional HTML routes to add additional pages
+
+# DONT TOUCH THIS ONE
+@app.route("/")
+def index(): 
+   return render_template("index.html")
+# ^DON'T TOUCH THIS ONE^
+
+# updated/refined geomap
+@app.route("/optimized_geomap")
+def optimized_geomap():
+    conn = psycopg2.connect(database="car_crash", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM "Optimized_Data";''')
+    refined_data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    refined_list = []
+    for item in refined_data:
+        refined_list.append({
+            "Date & Time": item[0],
+            "Weather": item[1],
+            "Latitude": item[2],
+            "Longitude": item[3],
+            "Vehicle Damage": item[4],
+            "Year": item[5],
+            "Make": item[6],
+            "Model": item[7],
+            "Substance Abuse": item[8],
+            "Injury Severity": item[9]
+        })
+    return jsonify(refined_list)
+
+
+
+
+
+# ROUTE FOR bubble PLOT using only crash_data
+@app.route("/bubble")
+def bubble():
+    conn = psycopg2.connect(database="car_crash", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+# create a cursor
+    cur = conn.cursor()
+# connect tables.
+    cur.execute('''SELECT * FROM "bubble_data";''')
+    bubble_data = cur.fetchall()
+    cur.close()
+    conn.close()
+    bubblelist = []
+    for item in bubble_data:
+        bubblelist.append([
+            item[0],
+            item[1],
+            item[2]
+        ])
+    # return render_template('index.html', data=data)
+    return jsonify(bubblelist)
+
+
+
+
+# ROUTES FOR LINE PLOT USING ONLY CRASH DATA
+
+# ROUTE FOR WEEK ONE DATA
+@app.route("/week_one")
+def line(): 
+    conn = psycopg2.connect(database="car_crash", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+# Create a cursor
+    cur = conn.cursor()
+# Connect to table for week one of accident data
+    cur.execute('''SELECT * FROM "week_one_totals";''')
+    week_one_data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    week_one_list = []
+    for item in week_one_data:
+        week_one_list.append({
+            "date_only": item[0],
+            "accident_count": item[1]
+        })
+    # Return render_template('index.html', data=data)
+    return jsonify(week_one_list)
+
+# ROUTE FOR WEEK TWO DATA
+@app.route("/week_two")
+def line(): 
+    conn = psycopg2.connect(database="car_crash", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+# Create a cursor
+    cur = conn.cursor()
+# Connect to table for week two of accident data
+    cur.execute('''SELECT * FROM "week_two_totals";''')
+    week_two_data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    week_two_list = []
+    for item in week_two_data:
+        week_two_list.append({
+            "date_only": item[0],
+            "accident_count": item[1]
+        })
+    # Return render_template('index.html', data=data)
+    return jsonify(week_two_list)
+
+# ROUTE FOR WEEK THREE DATA
+@app.route("/week_three")
+def line(): 
+    conn = psycopg2.connect(database="car_crash", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+# Create a cursor
+    cur = conn.cursor()
+# Connect to table for week three of accident data
+    cur.execute('''SELECT * FROM "week_three_totals";''')
+    week_three_data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    week_three_list = []
+    for item in week_three_data:
+        week_three_list.append({
+            "date_only": item[0],
+            "accident_count": item[1]
+        })
+    # Return render_template('index.html', data=data)
+    return jsonify(week_three_list)
+
+# ROUTE FOR WEEK FOUR DATA
+@app.route("/week_four")
+def line(): 
+    conn = psycopg2.connect(database="car_crash", user="postgres",
+                        password="postgres", host="localhost", port="5432")
+# Create a cursor
+    cur = conn.cursor()
+# Connect to table for week four of accident data
+    cur.execute('''SELECT * FROM "week_four_totals";''')
+    week_four_data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    week_four_list = []
+    for item in week_four_data:
+        week_four_list.append({
+            "date_only": item[0],
+            "accident_count": item[1]
+        })
+    # Return render_template('index.html', data=data)
+    return jsonify(week_four_list)
+
+# Connect to the database
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
